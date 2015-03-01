7ea0f89939e9c17d1728f8f4c71a1dc3a8d89468
Hetal Sakaria <hssakaria@csupomona.edu>
2015-03-01 01:40:37 -0800

    Assignemnt 9: I have used builder design pattern for the NewReleaseProb class. I have set prefix .with and also made changes so that json can set the data using builder class.

--- src/main/java/edu/csupomona/cs480/data/NewReleaseProb.java
+++ src/main/java/edu/csupomona/cs480/data/NewReleaseProb.java
@@ -7,59 +7,103 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
 /**
  * The basic user object.
  */
+
 public class NewReleaseProb {
 
 	private String filePath;
-
-	private String prob;
-	
+	private String problemId;
+	private int problemNo;
 	private String fileName;
 	private int week;
-	
+
+	@JsonCreator
+	public NewReleaseProb(
+			@JsonProperty("filePath") String filePath,
+			@JsonProperty("problemId") String problemId,
+			@JsonProperty("problemNo") int problemNo,
+			@JsonProperty("fileName") String fileName,
+			@JsonProperty("weekNo") int week)
+	{
+		this.fileName = fileName;
+		this.filePath = filePath;
+		this.problemId = problemId;
+		this.problemNo = problemNo;
+		this.week = week;
+	}
+
+	public NewReleaseProb(NewReleaseProblemBuilder newReleaseProbBuilder) {
+
+		this.fileName = newReleaseProbBuilder.fileName;
+		this.filePath = newReleaseProbBuilder.filePath;
+		this.problemId = newReleaseProbBuilder.problemId;
+		this.problemNo = newReleaseProbBuilder.problemNo;
+		this.week = newReleaseProbBuilder.week;
+
+	}
+
 	public String getFilePath() {
 		return filePath;
 	}
 
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
-	}
-
-	
-	public String getprob() {
-		return prob;
-	}
-
-	public void setprob(String prob) {
-		this.prob = prob;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
+	public String getproblemId() {
+		return problemId;
 	}
 
 	public String getFileName() {
 		return fileName;
 	}
 
-	public int getWeek() {
+	public int getweek() {
 		return week;
 	}
 
-	public void setWeek(int week) {
-		this.week = week;
+	public int getproblemNo() {
+
+		return problemNo;
 	}
 
-	
-
-	public static boolean isNumeric(String str) {
-		try {
-			int d = Integer.parseInt(str);
-			return true;
-		} catch (NumberFormatException nfe) {
-			return false;
+	public static class NewReleaseProblemBuilder {
+		private String filePath;
+		private String problemId;
+		private int problemNo;
+		private String fileName;		
+		private int week;
+		public NewReleaseProblemBuilder withproblemId(String problemId) {
+			this.problemId = problemId;
+			return this;
 		}
+
+		public NewReleaseProblemBuilder withfilePath(String filePath) {
+			this.filePath = filePath;
+			return this;
+		}
+
+		public NewReleaseProblemBuilder withfileName(String fileName) {
+			this.fileName = fileName;
+			return this;
+		}
+
+		public NewReleaseProblemBuilder withweekNo(int week) {
+			this.week = week;
+			return this;
+		}
+
+		public NewReleaseProblemBuilder withproblemNo(int problemNo) {
+			this.problemNo = problemNo;
+			return this;
+		}
+
+		public NewReleaseProb build() {
+			return new NewReleaseProb(this);
+		}
+
 	}
 }
